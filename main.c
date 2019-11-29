bb34badf7b7c7a5b6ae0daa542b64a04ec38d234
Mushbie <47923751+Mushbie@users.noreply.github.com>
2019-11-29 22:06:20 +0100

    Update main.c
    Started adding all the painfull USB stuff.

--- main.c
+++ main.c
@@ -2,6 +2,63 @@
 #include <stdlib.h>
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
+#include <libopencm3/usb/usbd.h>
+#include <libopencm3/usb/cdc.h>
+
+const struct usb_device_descriptor dev_desc = {
+	.bLenght = USB_DT_DEVICE_SIZE,
+	.bDescriptorType = USB_DT_DEVICE,
+	.bcdUSB = 0x0200,		// usb version number
+	.bDeviceClass = USB_CLASS_CDC,
+	.bDeviceSubClass = 0,
+	.bDeviceProtocol = 0,
+	.bMaxPacketSize0 = 64,
+	.idVendor = 0x0483,		// STM Vendor ID used while testing
+	.idProduct = 0x5740,	// STM virtuel serial ID
+	.bcdDevice = 0x0200,	// device version number
+	.iManufacturer = 1,		// manufacturer string index position
+	.iProduct = 2,			// product string index position
+	.iSerialNumber = 3,		// serial number string index position
+	.bNumConfigurations = 1,// number of configurations.
+};
+
+const char *usb_strings[] = {
+	"Mushbie Technology",
+	"floppyThing",
+	"DEMO",
+};
+
+// The cdc acm standard says this endpoint is optional,
+// but apparently the linux driver crashes without it.
+const struct usb_endpoint_desciptor notif_endpoint = {
+	.bLenght = USB_DT_ENDPOINT_SIZE,
+	.bDescriptorType = USB_DT_ENDPOINT,
+	.bEndpointAddress = 0x83,
+	.bmAttributes = USB_ENDPOINT_ATTR_INTERUPT,
+	.wMaxPacketSize = 16,
+	.bInterval = 255,
+};
+
+const struct usb_endpoint_desciptor data_endpoints[] = {
+	{
+		.bLenght = USB_DT_ENDPOINT_SIZE,
+		.bDescriptorType = USB_DT_ENDPOINT,
+		.bEndpointAddress = 0x01,
+		.bmAttributes = USB_ENDPOINT_ATTR_BULK,
+		.wMaxPacketSize = 64,
+		.bInterval = 1,
+	},
+	{
+		.bLenght = USB_DT_ENDPOINT_SIZE,
+		.bDescriptorType = USB_DT_ENDPOINT,
+		.bEndpointAddress = 0x82,
+		.bmAttributes = USB_ENDPOINT_ATTR_BULK,
+		.wMaxPacketSize = 64,
+		.bInterval = 1,
+	}
+};
+
+
 
 int main(void)
 {
