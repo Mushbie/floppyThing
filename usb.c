e012f89f5d3f72bd1729a22e1a7044c5fc96381d
Mads Thore Theodor Hansen <47923751+Mushbie@users.noreply.github.com>
2019-12-30 21:03:43 +0100

    Split off usb code.
    split off most of usb code to its own file, to clean up main.c

---
+++ usb.c
@@ -0,0 +1,164 @@
+const struct usb_device_descriptor device_desc = {
+	.bLength = USB_DT_DEVICE_SIZE,
+	.bDescriptorType = USB_DT_DEVICE,
+	.bcdUSB = 0x0200,		// usb version number
+	.bDeviceClass = USB_CLASS_CDC,
+	.bDeviceSubClass = 0,
+	.bDeviceProtocol = 0,
+	.bMaxPacketSize0 = 64,
+	.idVendor = 0x0483,		// STM Vendor ID used while testing
+	.idProduct = 0x5740,	// STM virtuel serial ID
+	.bcdDevice = 0x0200,	// device version number
+	.iManufacturer = 1,		// manufacturer string index position
+	.iProduct = 2,			// product string index position
+	.iSerialNumber = 3,		// serial number string index position
+	.bNumConfigurations = 1,// number of configurations.
+};
+
+const char *usb_strings[] = {
+	"Mushbie Technology",
+	"floppyThing",
+	"DEMO",
+};
+
+
+/* Libopencm3 example code claims this endpoint is optional,
+in the cdc acm standard. As far a I can see it is not, and
+apparently the linux driver crashes without it. */
+const struct usb_endpoint_descriptor notification_endpoint = {
+	.bLength = USB_DT_ENDPOINT_SIZE,
+	.bDescriptorType = USB_DT_ENDPOINT,
+	.bEndpointAddress = 0x83,
+	.bmAttributes = USB_ENDPOINT_ATTR_INTERRUPT,
+	.wMaxPacketSize = 16,
+	.bInterval = 255,
+};
+
+const struct usb_endpoint_descriptor data_endpoints[] = {
+	{
+		.bLength = USB_DT_ENDPOINT_SIZE,
+		.bDescriptorType = USB_DT_ENDPOINT,
+		.bEndpointAddress = 0x01,
+		.bmAttributes = USB_ENDPOINT_ATTR_BULK,
+		.wMaxPacketSize = 64,
+		.bInterval = 1,
+	},
+	{
+		.bLength = USB_DT_ENDPOINT_SIZE,
+		.bDescriptorType = USB_DT_ENDPOINT,
+		.bEndpointAddress = 0x82,
+		.bmAttributes = USB_ENDPOINT_ATTR_BULK,
+		.wMaxPacketSize = 64,
+		.bInterval = 1,
+	}
+};
+
+const struct {
+	struct usb_cdc_header_descriptor header;
+	struct usb_cdc_acm_descriptor acm;
+	struct usb_cdc_call_management_descriptor call_mngmnt;
+	struct usb_cdc_union_descriptor cdc_union;
+} __attribute__((packed)) cdc_functional_descriptors =
+{
+	.header = {
+		.bFunctionLength = sizeof(struct usb_cdc_header_descriptor),
+		.bDescriptorType = CS_INTERFACE,
+		.bDescriptorSubtype = USB_CDC_TYPE_HEADER,
+		.bcdCDC = 0x0110,	// version of the cdc standard
+	},
+	.acm = {
+		.bFunctionLength = sizeof(struct usb_cdc_acm_descriptor),
+		.bDescriptorType = CS_INTERFACE,
+		.bDescriptorSubtype = USB_CDC_TYPE_ACM,
+		.bmCapabilities = 0,
+	},
+	.call_mngmnt = {
+		.bFunctionLength = sizeof(struct usb_cdc_call_management_descriptor),
+		.bDescriptorType = CS_INTERFACE,
+		.bDescriptorSubtype = USB_CDC_TYPE_CALL_MANAGEMENT,
+		.bmCapabilities = 0,
+		.bDataInterface = 1,
+	},
+	.cdc_union = {
+		.bFunctionLength = sizeof(struct usb_cdc_union_descriptor),
+		.bDescriptorType = CS_INTERFACE,
+		.bDescriptorSubtype = USB_CDC_TYPE_UNION,
+		.bControlInterface = 0,
+		.bSubordinateInterface0 = 1,
+	}
+};
+
+const struct usb_interface_descriptor com_interface = {
+	.bLength = USB_DT_INTERFACE_SIZE,
+	.bDescriptorType = USB_DT_INTERFACE,
+	.bInterfaceNumber = 0,
+	.bAlternateSetting = 0,
+	.bNumEndpoints = 1,
+	.bInterfaceClass = USB_CLASS_CDC,
+	.bInterfaceSubClass = USB_CDC_SUBCLASS_ACM,
+	.bInterfaceProtocol = USB_CDC_PROTOCOL_AT,
+	.iInterface = 0,
+	.endpoint = &notification_endpoint,
+	.extra = &cdc_functional_descriptors,
+	.extralen = sizeof(cdc_functional_descriptors)
+};
+
+const struct usb_interface_descriptor data_interface = {
+	.bLength = USB_DT_INTERFACE_SIZE,
+	.bDescriptorType = USB_DT_INTERFACE,
+	.bInterfaceNumber = 1,
+	.bAlternateSetting = 0,
+	.bNumEndpoints = 2,
+	.bInterfaceClass = USB_CLASS_DATA,
+	.bInterfaceSubClass = 0,
+	.bInterfaceProtocol = 0,
+	.iInterface = 0,
+	.endpoint = data_endpoints
+};
+
+const struct usb_interface interfaces[] = {
+	{
+		.num_altsetting = 1,
+		.altsetting = &com_interface,
+	},
+	{
+		.num_altsetting = 1,
+		.altsetting = &data_interface,
+	}
+};
+
+const struct usb_config_descriptor configuration_desc = {
+	.bLength = USB_DT_CONFIGURATION_SIZE,
+	.bDescriptorType = USB_DT_CONFIGURATION,
+	.wTotalLength = 0,
+	.bNumInterfaces = 2,
+	.bConfigurationValue = 1,
+	.iConfiguration = 0,
+	.bmAttributes = 0x80, // bus powered
+	.bMaxPower = 0x32,
+	.interface = interfaces,
+};
+
+enum usbd_request_return_codes cdcacm_request_handler(usbd_device *device,
+	struct usb_setup_data *request, uint8_t **buffer, uint16_t *length,
+	void(**complete)(usbd_device *device, struct usb_setup_data *request))
+{
+	// tell the compiler these valriable not used
+	(void)device;
+	(void)buffer;
+	(void)complete;
+	
+	switch(request->bRequest)
+	{
+		case USB_CDC_REQ_SET_CONTROL_LINE_STATE:
+			return USBD_REQ_HANDLED;
+		case USB_CDC_REQ_SET_LINE_CODING:
+			if(*length < sizeof(struct usb_cdc_line_coding))
+			{
+				return USBD_REQ_NOTSUPP;
+			}
+			return USBD_REQ_HANDLED;
+		default:
+			return USBD_REQ_NOTSUPP;
+	}
+}
