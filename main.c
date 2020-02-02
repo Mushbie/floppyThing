e23909781121b1c5752c2000faac54e030183b60
Mads Thore Theodor Hansen <47923751+Mushbie@users.noreply.github.com>
2020-02-02 22:31:52 +0100

    Added license
    Added the GNU GPLv3 license.

--- main.c
+++ main.c
@@ -1,3 +1,20 @@
+/*
+	This file is part of floppyThing, a floppy imaging tool.
+	Copyright 2020 Mads Thore Theodor Hansen
+
+    floppyThing is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    floppyThing is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with floppyThing.  If not, see <https://www.gnu.org/licenses/>.
+*/
 
 #include <stdlib.h>
 #include <libopencm3/stm32/rcc.h>
